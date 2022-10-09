508e9f8d2e842b35859dd7956586d76bfdb6034b
Krissy Macdonald <krissymacdonald5@gmail.com>
2022-10-08 21:01:49 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to Spookytown!")
 })
 
+// Greetings
 app.get("/greeting/", (req, res) => {
     res.send("Hello, stranger.")
 })
@@ -13,6 +14,10 @@ app.get("/greeting/:name", (req, res) => {
     res.send(`Howdy, ${req.params.name}`)
 })
 
+// Tip Calculator
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send(`Your tip will be $${ (parseInt(req.params.total) * parseInt(req.params.tipPercentage)) / 100}`)
+})
 
 
 
